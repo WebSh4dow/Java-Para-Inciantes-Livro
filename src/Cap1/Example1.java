43b9a466bcc146a9a27103c0009c65e815adb883
WebSh4dow <phantomrising@protonmail.com>
2023-10-18 18:48:49 -0300

    commit inicial, cap 1 exemplos do livro

---
+++ src/Cap1/Example1.java
@@ -0,0 +1,15 @@
+package Cap1;
+
+/*
+ Este é um programa Java simples.
+
+ Chame este arquivo de Example.java
+
+->  Isto é um comentário de várias linhas
+*/
+public class Example1 {
+    // Um programa Java começa com uma chamada a main() -> comentário de linha unica
+    public static void main(String args[]){
+        System.out.println("Java drives the Web.");
+    }
+}
---
+++ src/Cap1/Example2.java
@@ -0,0 +1,25 @@
+package Cap1;
+
+public class Example2 {
+    /*
+       Este código demonstra uma variável.
+       Chame este arquivo de Cap1.Example2.java
+    */
+    public static void main(String args[]){
+        int variavel1; // esta instrução declara uma variável
+        int variavel2; // esta instrução declara outra variável
+
+        variavel1 = 1024; // esta variavel atribui 1024 a variavel1
+
+        System.out.println("Variavel1 contains: " + variavel1);
+        variavel2 = variavel1 / 2;
+
+        System.out.println("Variavel2 contains variavel1 / 2: " + variavel2);
+        System.out.println(variavel2);
+
+
+
+
+
+    }
+}
